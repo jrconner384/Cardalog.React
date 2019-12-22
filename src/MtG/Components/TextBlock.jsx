e8212494f6ed6403bd2eac12be808c54c2686091
Jason Conner <jason.r.conner@gmail.com>
2019-12-21 18:46:33 -0600

    Disabling a warning about useless constructors in a few places.

--- src/MtG/Components/TextBlock.jsx
+++ src/MtG/Components/TextBlock.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 class TextBlock extends React.Component {
+  // According to React, classes should always call the base constructor (https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class)
+  // but the linter doesn't like it. Disabling the warning for now.
+  // eslint-disable-next-line
   constructor(props) {
     super(props);
   }
